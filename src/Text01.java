a7485df283664c68c525fad4ecd0c39fa82e14f8
ALUNO <ALUNO@LAB04-PC12>
2024-06-04 21:23:45 -0300

    ffmv

---
+++ src/Text01.java
@@ -0,0 +1,25 @@
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        
+        System.out.println("    xxxxxxxxxxxx   ");
+        System.out.println("  x               x ");
+        System.out.println("x    xx       xx   x");
+        System.out.println("x     XX      xx   x");
+        System.out.println("x                  x");
+        System.out.println("x                  x");
+        System.out.println("x    x         x   x");
+        System.out.println("x     x      x     x");
+        System.out.println("x        xxx       x");
+        System.out.println("  x             x");
+        System.out.println("   xxxxxxxxxxxx    ");
+        
+    }    
+}
